27b7cd8b10325118ad838ab62ecc5b70d72897dd
Alexande6055 <juanperzezss@gmail.com>
2025-12-13 20:36:19 -0500

    instalacion de tailwindcss

--- exercise1/front-end/src/pages/Home.tsx
+++ exercise1/front-end/src/pages/Home.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
-    return (
-        <div>
-            <h1>
-                Hellow World!!
-            </h1>
-        </div>
-    )
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 to-purple-600">
+      <h1 className="text-5xl font-extrabold text-white drop-shadow-md">
+        Hello World!!
+      </h1>
+    </div>
+  )
 }
